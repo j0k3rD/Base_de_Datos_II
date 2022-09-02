f2f543b879d5bd91b04ab9959a81618135341e4b
j0k3rD <agm.moya@alumno.um.edu.ar>
2022-09-02 00:24:59 -0300

    practic started

---
+++ Prac/practica_consultas_sakila.sql
@@ -0,0 +1,27 @@
+use sakila;
+
+select R.rental_id "ID_ALQUILER",
+	R.rental_date "DIA_ALQUILER",
+    R.inventory_id "ID_INVENTARIO",
+    (Select Fi.title from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "TITULO_PELICULA",
+    (Select Fi.description from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "DESCRIPCION",
+    (Select Fi.release_year from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "AÑO",
+    (Select L.name from language L where L.language_id = (Select Fi.language_id from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id))) "LENGUAJE",
+    (Select Fi.original_language_id from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "LENGUAJE_ORIGINAL",
+    (Select Fi.rental_duration from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "TIEMPO_RENTA",
+    (Select Fi.rating from film Fi where Fi.film_id = (Select I.film_id from inventory I where  R.inventory_id = I.inventory_id)) "CLASIFICACION",
+    (Select Ad.address from address Ad where Ad.address_id = (Select St.address_id from store St where St.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id))) "DIRECCION",
+    (Select Ci.city from city Ci where Ci.city_id = (Select Ad.city_id from address Ad where Ad.address_id = (Select St.address_id from store St where St.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id)))) "CITY",
+	(Select Ad.postal_code from address Ad where Ad.address_id = (Select St.address_id from store St where St.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id))) "CODIGO_POSTAL",
+	(Select Ad.phone from address Ad where Ad.address_id = (Select St.address_id from store St where St.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id))) "TELEFONO",
+    (Select Cu.first_name from customer Cu where R.customer_id = Cu.customer_id) "NOMBRE_CLIENTE",
+    (Select Cu.last_name from customer Cu where R.customer_id = Cu.customer_id) "APELLIDO_CLIENTE",
+    (Select Cu.email from customer Cu where R.customer_id = Cu.customer_id) "EMAIL_CLIENTE",
+    (Select first_name from staff S where S.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id)) "NOMBRE_PERSONAL",
+    (Select last_name from staff S where S.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id)) "APELLIDO_PERSONAL",
+    (Select email from staff S where S.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id)) "EMAIL_PERSONAL",
+	(Select username from staff S where S.store_id = (Select I.store_id from inventory I where R.inventory_id = I.inventory_id)) "USERNAME",
+    R.customer_id "ID_CLIENTE",
+    R.return_date "DIA_DEVOLUCION",
+    R.last_update "ULTIMA ACTUALIZACION"
+from rental R;
